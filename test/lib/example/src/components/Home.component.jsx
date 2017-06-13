0cfe924765ee39003228efb0e6cfbd904ffe65ee
Inumidun Amao <inumidun.amao@andela.com>
2017-06-13 22:02:21 +0100

    fix failing test and travis build issues

---
+++ test/lib/example/src/components/Home.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/**
+ * Home page view
+ * @class Home
+ * @extends {React.Component}
+ */
+class Home extends React.Component {
+
+  /**
+   * Renders the view of the component
+   * @returns {Object} react component to render
+   * @memberOf Home
+   */
+  render() {
+    return (
+      <h2>Home page</h2>
+    );
+  }
+}
+
+export default Home;
